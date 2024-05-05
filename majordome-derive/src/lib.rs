76a6b38bf8224bd333ab779efa53f382571178c3
merlleu <r.langdorph@gmail.com>
2024-05-05 23:38:20 +0200

    improve doc for majordome-derive

--- majordome-derive/src/lib.rs
+++ majordome-derive/src/lib.rs
@@ -1,5 +1,43 @@
 mod majordome_errors;
 
+/// Derive macro for the `IntoMajordomeError` trait.
+/// Convert an enum to a MajordomeError.
+/// Enum Attributes:
+/// - `prefix`: Prefix for the error code. Required.
+/// Enum Variants Attributes:
+/// - `code`: Error code. Required.
+/// - `msg`: Error message. Required. The string is formatted using enum variant fields.
+/// - `status`: HTTP status code. Optional. Default is 500.
+/// 
+/// # Example
+/// ```rs
+/// #[derive(MajordomeError)]
+/// #[err(prefix = "errors.gg.wls.")]
+/// pub enum AuthError {
+///     #[err(code="invalid_token", msg="Invalid token", status=401)]
+///     InvalidToken,
+///
+///     #[err(code="unknown_event", msg="Unknown event {id}", status=404)]
+///     UnknownEvent {id: String},
+/// 
+///     #[err(code="not_enough_players", msg="Not enough players (required: {required}, actual: {actual})", status=400)]
+///     NotEnoughPlayers{required: u32, actual: u32},
+/// }
+/// ```
+/// 
+/// Into/From are implemented for MajordomeError.
+/// ```rs
+/// AuthError::UnknownEvent{id: "123".to_string()}.into()
+/// ```
+/// equals to
+/// ```rs
+/// MajordomeError::new(
+///     "errors.gg.wls.unknown_event".to_string(),
+///     "Unknown event 123".to_string(),
+///     vec!["123".to_string()],
+///     404
+/// )
+/// ``````
 #[proc_macro_derive(IntoMajordomeError, attributes(err))]
 pub fn into_majordome_error_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     majordome_errors::parse_enum_error(input)
