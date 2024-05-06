c3785d70221a1c5a3f5603bd4d5279efa8b983e9
merlleu <r.langdorph@gmail.com>
2024-05-06 02:30:28 +0200

    fix recursion in axum intoresponse from error

--- majordome/src/error/axum.rs
+++ majordome/src/error/axum.rs
@@ -1,8 +1,10 @@
+use axum::Json;
+
 use crate::MajordomeError;
 
 impl axum::response::IntoResponse for MajordomeError {
     fn into_response(self) -> axum::http::Response<axum::body::Body> {
         let status = axum::http::StatusCode::from_u16(self.status_code).unwrap();
-        (status, self).into_response()
+        (status, Json(self)).into_response()
     }
 }
